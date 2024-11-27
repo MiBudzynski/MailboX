d62d61b9ddb6aa7dcd7cd80c992732289b336133
AKashyna <agata.kashyna@gmail.com>
2024-11-27 22:29:18 +0100

    client pass cpp

--- client.c
+++ client.c
@@ -2,18 +2,21 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <cstring>
 #include <unistd.h>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     struct sockaddr_in sa;
     int SocketFD;
-    int port=1100;//atoi(argv[2]);
-    printf ("addr: %s\n",argv[1]);
-    printf ("port: %i\n",port);
+    int port = 1100; // atoi(argv[2]);
+    cout << "addr: " << argv[1] << endl;
+    cout << "port: " << port << endl;
+    
     SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (SocketFD == -1) {
         perror("cannot create socket");
@@ -22,51 +25,43 @@ int main(int argc, char *argv[])
 
     memset(&sa, 0, sizeof sa);
 
-    char login[256];
-    bzero(login, 256);
-    char password[256];
-    bzero(password);
+    string login;
+    string password;
 
-    sa.sin_addr.s_addr=inet_addr(argv[1]);
+    sa.sin_addr.s_addr = inet_addr(argv[1]);
     sa.sin_family = AF_INET;
     sa.sin_port = htons(port);
 
-    printf("Podaj login: \n");
-    fgets(login, sizeof login, stdin);
-    printf("Podaj hasło:\n");
-    fgets(password, sizeof password, stdin);
+    cout << "Podaj login: " << endl;
+    getline(cin, login); // fgets(login, sizeof login, stdin);
+    cout << "Podaj hasło: " << endl;
+    getline(cin, password); // fgets(password, sizeof password, stdin);
 
     if (connect(SocketFD, (struct sockaddr *)&sa, sizeof sa) == -1) {
         perror("connect failed");
         close(SocketFD);
         exit(EXIT_FAILURE);
     }
-    else
-    {
-        printf("Connection accepted \n");
+    else {
+        cout << "Connection accepted" << endl;
     }
+
     char buff[256];
     char buff_rcv[256];
 
+    while (true) {
+        memset(buff, 0, sizeof(buff));
+        memset(buff_rcv, 0, sizeof(buff_rcv));
 
-    while(1)
-    {
-        bzero(buff, 256);
-        bzero(buff_rcv,256);
-
-
-        if(read(SocketFD,buff_rcv,sizeof buff_rcv) == 0)
-        {
+        if (read(SocketFD, buff_rcv, sizeof(buff_rcv)) == 0) {
             break;
-        }
-        else
-        {
-            printf("recived :%s \n", buff_rcv);
-            scanf("%s",buff);
-            write(SocketFD,buff,sizeof buff);
+        } else {
+            cout << "received: " << buff_rcv << endl;
+            cin >> buff;
+            write(SocketFD, buff, sizeof(buff));
         }
     }
+
     close(SocketFD);
     return EXIT_SUCCESS;
 }
-
