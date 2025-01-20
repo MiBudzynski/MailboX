5bab9be0ed807e91f6c381dbf946568a70323a11
MiBudzynski <michal.budzynski8@gmail.com>
2025-01-20 16:51:13 +0100

    wstepnie done, przed testami

--- server.cpp
+++ server.cpp
@@ -11,13 +11,14 @@
 #include <pthread.h>
 #include "Database.h"
 
+const vector<string> ips = {"10.0.2.15"};
 using namespace std;
 
-void connectToOtherServers(const std::vector<std::string>& ips) {
+void connectToOtherServers(string option, string sender, string receiver, string other, string subject) {
     for (const auto& ip : ips) {
         int client_socket = socket(AF_INET, SOCK_STREAM, 0);
         if (client_socket < 0) {
-            std::cerr << "Error creating socket for " << ip << std::endl;
+            cerr << "Error creating socket for " << ip << endl;
             continue;
         }
 
@@ -27,21 +28,73 @@ void connectToOtherServers(const std::vector<std::string>& ips) {
         server_addr.sin_port = htons(1100);
 
         if (inet_pton(AF_INET, ip.c_str(), &server_addr.sin_addr) <= 0) {
-            std::cerr << "Invalid IP address: " << ip << std::endl;
+            cerr << "Invalid IP address: " << ip << endl;
             close(client_socket);
             continue;
         }
 
         if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-            std::cerr << "Could not connect to " << ip << std::endl;
+            cerr << "Could not connect to " << ip << endl;
             close(client_socket);
             continue;
         }
 
-        const char* message = "server";
-        send(client_socket, message, strlen(message), 0);
-        std::cout << "Connected and sent message to " << ip << std::endl;
+        if (write(client_socket, "server", 7) < 0){
+            perror("Error writing to socket");
+        }   
+        cout << "Connected to " << ip << endl;
 
+        if (option == "synchronise"){
+            usleep(100000);
+            if (write(client_socket, option.c_str(), option.length()) <= 0) {
+                perror("Error sending option");
+                close(client_socket);
+            }
+            //TODO
+            cout << "Synhronized with " << ip << endl;
+        }else if(option == "New User"){
+            usleep(100000);
+            if (write(client_socket, option.c_str(), option.length()) <= 0) {
+                perror("Error sending option");
+                close(client_socket);
+            }
+            usleep(100000);
+            if (write(client_socket, sender.c_str(), sender.length()) <= 0) {
+                perror("Error sending sender");
+                close(client_socket);
+            }
+            usleep(100000);
+            if (write(client_socket, other.c_str(), other.length()) <= 0) {
+                perror("Error sending password");
+                close(client_socket);
+            }
+        }else if(option == "New Message"){
+            usleep(100000);
+            if (write(client_socket, option.c_str(), option.length()) <= 0) {
+                perror("Error sending option");
+                close(client_socket);
+            }
+            usleep(100000);
+            if (write(client_socket, sender.c_str(), sender.length()) <= 0) {
+                perror("Error sending sender");
+                close(client_socket);
+            }
+            usleep(100000);
+            if (write(client_socket, receiver.c_str(), receiver.length()) <= 0) {
+                perror("Error sending receiver");
+                close(client_socket);
+            }
+            usleep(100000);
+            if (write(client_socket, subject.c_str(), subject.length()) <= 0) {
+                perror("Error sending subject");
+                close(client_socket);
+            }
+            usleep(100000);
+            if (write(client_socket, other.c_str(), other.length()) <= 0) {
+                perror("Error sending content");
+                close(client_socket);
+            }
+        }
         close(client_socket);
     }
 }
@@ -88,6 +141,7 @@ void clients(int newSocket){
             break;
         if(option == "rejestruj" && !czyIstniejeUzytkownik(login)){
             if (!createAccount(login, password)) cerr << "Error adding new account\n";
+            connectToOtherServers("New User", login, "", password, "");
             break;
         }
         if (write(newSocket, "Access denied", 14) < 0)
@@ -141,18 +195,19 @@ void clients(int newSocket){
                 cerr << "Client disconnected or error reading\n";
                 break;
             }
-            cout << "Received reciver: " << receiver << std::endl;
+            cout << "Received reciver: " << receiver << endl;
             if (read(newSocket, subject, sizeof(subject)) <= 0) {
                 cerr << "Client disconnected or error reading\n";
                 break;
             }
-            cout << "Received topic: " << subject << std::endl;
+            cout << "Received topic: " << subject << endl;
             if (read(newSocket, content, sizeof(content)) <= 0) {
                 cerr << "Client disconnected or error reading\n";
                 break;
             }
-            cout << "Received message: " << content << std::endl;
+            cout << "Received message: " << content << endl;
             addMessage(login, receiver, subject, content);
+            connectToOtherServers("New Message", login, receiver, content, subject);
         }else{
              cerr << "Unknown acction\n";
         }
@@ -160,7 +215,74 @@ void clients(int newSocket){
 }
 
 void servers(int newSocket){
-    cout << "Im am in other server " << endl;
+    cout << "Other server connect to me :)" << endl;
+    char option[256];
+    memset(option, 0, 256);
+    // Odbiór opcji wejscia
+    if (read(newSocket, option, sizeof(option)) <= 0) {
+        cerr << "Error reading option\n";
+        close(newSocket);
+        pthread_exit(NULL);
+    }
+    cout << "Server chose option: " << option << endl;
+
+    if (strcmp(option, "synchronise") == 0 ){
+        //TODO
+        cout << "Synhronized with other server" << endl;
+    }else if(strcmp(option, "New User") == 0){
+        char login[256], password[256];
+        memset(login, 0, 256);
+        memset(password, 0, 256);
+        // Odbiór loginu
+        if (read(newSocket, login, 256) <= 0) {
+            cerr << "Error reading login\n";
+            close(newSocket);
+            pthread_exit(NULL);
+        }
+        cout << "Received login: " << login << endl;
+
+        // Odbiór hasła
+        if (read(newSocket, password, 256) <= 0) {
+            cerr << "Error reading password\n";
+            close(newSocket);
+            pthread_exit(NULL);
+        }
+        cout << "Received password: " << password << endl;
+        if (!createAccount(login, password)) cerr << "Error adding new account\n";
+        cout << "Added new user from other server" << endl;
+    }else if(strcmp(option, "New Message") == 0){
+        char sender[256], receiver[256], subject[256], content[1000];
+        memset(sender, 0, 256);
+        memset(receiver, 0, 256);
+        memset(subject, 0, 256);
+        memset(content, 0, 1000);
+        // Odbiór nadawcy
+        if (read(newSocket, sender, 256) <= 0) {
+            cerr << "Error reading login\n";
+            close(newSocket);
+            pthread_exit(NULL);
+        }
+        // Odbiór odbiorcy
+        if (read(newSocket, receiver, 256) <= 0) {
+            cerr << "Error reading password\n";
+            close(newSocket);
+            pthread_exit(NULL);
+        }
+        // Odbiór tematu
+        if (read(newSocket, subject, 256) <= 0) {
+            cerr << "Error reading subject\n";
+            close(newSocket);
+            pthread_exit(NULL);
+        }
+        // Odbiór tresci
+        if (read(newSocket, content, 1000) <= 0) {
+            cerr << "Error reading content\n";
+            close(newSocket);
+            pthread_exit(NULL);
+        }
+        addMessage(sender, receiver, subject, content);
+        cout << "Added new message from other server" << endl;
+    }
 }
 
 void *socketThread(void *arg) {
@@ -210,8 +332,7 @@ int main() {
         cerr << "Error creating Database";
     }
 
-    vector<string> ips = {"10.0.2.15"};
-    connectToOtherServers(ips);
+    connectToOtherServers("synchronise", "", "", "", "");
 
     pthread_t thread_id;
     // Pętla do dalszej komunikacji
